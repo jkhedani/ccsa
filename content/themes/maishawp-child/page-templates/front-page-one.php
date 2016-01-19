368b019033a30b054ca84e2eabf21b97aada48c7
Justin Hedani <jkhedani@gmail.com>
2016-01-19 13:51:43 -1000

    Update home call to action.

--- content/themes/maishawp-child/page-templates/front-page-one.php
+++ content/themes/maishawp-child/page-templates/front-page-one.php
@@ -93,10 +93,57 @@ get_header(); ?>
         </div>
     </div>
 
+    <!-- Footer Call To Action -->
+    <?php $posts = get_field('footer_call_out'); ?>
+    <?php if( $posts ): ?>
+        <div class="second-block">
+            <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+                <?php setup_postdata($post); ?>
 
-    <?php maisha_featured_page_one(); ?>
-	<?php maisha_featured_page_two(); ?>
-	<?php maisha_featured_page_three(); ?>
+                <?php $image_id = get_post_thumbnail_id(); ?>
+                <?php $image_url = wp_get_attachment_image_src($image_id,'full');   ?>
+                <div class="block-two" style="background-image:url(<?php echo esc_url( $image_url[0] ); ?>);"><div class="overlay"></div>
+                    <div class="hfeed site block-two-content">
+                        <div class="content site-content">
+                            <div class="content-area">
+                                <div class="main site-main" role="main">
+                                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                        <div class="entry-content">
+                                            <?php
+                                                /* translators: %s: Name of current post */
+                                                the_content( sprintf(
+                                                wp_kses( __( 'Continue reading %s', 'maisha' ), array( 'span' => array( 'class' => array() ) ) ),
+                                                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                                                ) );
+                                            ?>
+                                            <?php
+                                                wp_link_pages( array(
+                                                    'before'      => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'maisha' ) . '</span>',
+                                                    'after'       => '</div>',
+                                                    'link_before' => '<span>',
+                                                    'link_after'  => '</span>',
+                                                    'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'maisha' ) . ' </span>%',
+                                                    'separator'   => '<span class="screen-reader-text">, </span>',
+                                                ) );
+                                            ?>
+                                        </div><!-- .entry-content -->
+                                    </article><!-- #post-## -->
+                                   </div><!-- .site-main -->
+                               </div><!-- .content-area -->
+                           </div><!-- .site-content -->
+                       </div><!-- .site -->
+                   </div><!-- .block-two -->
+
+
+            <?php endforeach; ?>
+        </div><!-- .second-block -->
+        <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+    <?php endif; ?>
+
+
+    <?php //maisha_featured_page_one(); ?>
+	<?php //maisha_featured_page_two(); ?>
+	<?php //maisha_featured_page_three(); ?>
     <?php if ( is_active_sidebar( 'sidebar-7' ) ) : ?>
      <div class="block-four">
         <div class="columns clear">
