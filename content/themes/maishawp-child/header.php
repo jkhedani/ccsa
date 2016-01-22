618854ddcd9aac269dd8c80331a5306748871c43
Justin Hedani <jkhedani@gmail.com>
2016-01-21 15:09:27 -1000

    Update content.

--- content/themes/maishawp-child/header.php
+++ content/themes/maishawp-child/header.php
@@ -19,6 +19,16 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!-- <script type="text/javascript" src="https://www.votervoice.net/Scripts/PAMAAAEAAAA/Plugin.js"></script> -->
 	<?php wp_head(); ?>
+	<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-20854919-1', 'auto');
+ga('send', 'pageview');
+
+</script>
 </head>
 
 <body <?php body_class(); ?>>
